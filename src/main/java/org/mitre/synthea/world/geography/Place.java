softcraftsman/synthea

Move serialization test code to test class. Add 'implements Serializable' to classes used in patient.attributes and mark attributes that cannot be serialized as 'transient'.

src/main/java/org/mitre/synthea/world/geography/Place.java
@@ -1,5 +1,6 @@
 package org.mitre.synthea.world.geography;
 
+import java.io.Serializable;
 import java.util.Map;
 
 import org.apache.sis.geometry.DirectPosition2D;
@@ -8,7 +9,7 @@ import org.apache.sis.index.tree.QuadTreeData;
 /**
  * Place represents a named place with a postal code and coordinate.
  */
-public class Place implements QuadTreeData {
+public class Place implements QuadTreeData, Serializable {
   /** The name of the state. For example, Ohio */
   public String state;
   /** The state abbreviation. For example, OH */

dataweave19/synthea-healthcare-etl

src/main/java/org/mitre/synthea/world/geography/Place.java
@@ -1,6 +1,7 @@
 package org.mitre.synthea.world.geography;
 
 import java.awt.geom.Point2D;
+import java.io.Serializable;
 import java.util.Map;
 
 import org.mitre.synthea.world.geography.quadtree.QuadTreeElement;
@@ -8,7 +9,7 @@ import org.mitre.synthea.world.geography.quadtree.QuadTreeElement;
 /**
  * Place represents a named place with a postal code and coordinate.
  */
-public class Place implements QuadTreeElement {
+public class Place implements QuadTreeElement, Serializable {
   /** The name of the state. For example, Ohio */
   public String state;
   /** The state abbreviation. For example, OH */